4bfc537ffff191733a331e6cb3a4e682a3768a29
Mrrebrik <itibsoft@gmail.com>
2022-08-08 03:26:12 +0700

    - base join/leave chat users

--- bot.js
+++ bot.js
@@ -4,7 +4,22 @@ const { Telegraf, Markup, Scenes, Stage, session } = require('telegraf');
 
 const bot = new Telegraf(process.env.TOKEN_BOT);
 
+//Прослушивание сообщений в чате
+bot.on('text', (ctx) => {
+    console.log(ctx);
+    ctx.reply(`Hello ${ctx.message.text}`)
+});
 
+//Новый пользователь в чате
+bot.on('new_chat_members', (ctx) => {
+    ctx.reply(`Добро пожаловать в чат!`);
+    console.log(ctx.message.new_chat_members)
+});
+
+bot.on('left_chat_member', (ctx) => {
+    ctx.reply(`Ну и пошел он лесом :) Нам и без него хорошо!`);
+    console.log(ctx.message.left_chat_member)
+});
 
 bot.launch();
 console.log("Started telegram bot!")
